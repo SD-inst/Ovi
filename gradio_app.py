dc5afea5bb6b2bcf7e392cb31887f656890c1ed5
rkfg <rkfg@rkfg.me>
2025-10-04 11:59:42 +0300

    8 bit quantization support

--- gradio_app.py
+++ gradio_app.py
@@ -22,6 +22,11 @@ parser.add_argument(
     action="store_true",
     help="Enable CPU offload for both OviFusionEngine and FluxPipeline",
 )
+parser.add_argument(
+    "--fp8",
+    action="store_true",
+    help="Enable 8 bit quantization of the fusion model",
+)
 args = parser.parse_args()
 
 
@@ -36,7 +41,8 @@ def leave():
 # Initialize OviFusionEngine
 enable_cpu_offload = args.cpu_offload or args.use_image_gen
 use_image_gen = args.use_image_gen
-print(f"loading model... {enable_cpu_offload=}, {use_image_gen=} for gradio demo")
+fp8 = args.fp8
+print(f"loading model... {enable_cpu_offload=}, {use_image_gen=}, {fp8=} for gradio demo")
 DEFAULT_CONFIG["cpu_offload"] = (
     enable_cpu_offload  # always use cpu offload if image generation is enabled
 )
@@ -48,7 +54,7 @@ requests.post(
     json={"unload_models": "true", "free_memory": "true"},
     timeout=5,
 )
-ovi_engine = OviFusionEngine()
+ovi_engine = OviFusionEngine(fp8=fp8)
 flux_model = None
 if use_image_gen:
     flux_model = FluxPipeline.from_pretrained(
